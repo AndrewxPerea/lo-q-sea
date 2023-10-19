696a36d4e6724a25e052bf0b644a08ebf6906b63
Andrewx Perea <104550018+AndrewxPerea@users.noreply.github.com>
2023-10-18 21:07:13 -0500

    primer app

---
+++ .anaconda/navigator/scripts/anaconda-navigator-app.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\pc\anaconda3\Scripts\activate C:\Users\pc\anaconda3
+anaconda-navigator >C:\Users\pc\.anaconda\navigator\scripts\anaconda-navigator-app-out-1.txt 2>C:\Users\pc\.anaconda\navigator\scripts\anaconda-navigator-app-err-1.txt
